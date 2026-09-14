f9df8936fcc710eded47d41cd5e2b5175bae8aac
PEDRO HENRIQUE DA SILVA <55506799810@info.local>
2026-09-14 09:29:39 -0300

    Aprendendo funções basicas do JS

---
+++ script.js
@@ -0,0 +1,161 @@
+
+//! Funções Nativas JS
+
+//! Função MAP
+// Percorre uma lista e cria uma nova  lista com base na condição.
+/*
+let numeros = [1, 2, 3, 4];
+
+let numerosDobrados = numeros.map(function (num) {
+    return num * 2;
+})
+
+//* Retorno -> [2, 4, 6, 8]
+
+console.log(numerosDobrados);
+
+// Ou assim:
+// Arrow function com retorno implicito
+
+let numerosDobradosArrow = numeros.map(num => num * 2);
+
+//? Quando eu preciso usar o return?
+//* Quando a função tem mais de uma linha
+// A setinha resume o function
+
+numeros.map(num => {
+    let resultado = num * 2;
+    return resultado;
+})
+
+console.log("resultado " + resultado);
+*/
+
+//! Função FILTER
+// Filtra elemontos com base em uma condição
+/*
+let numeros = [5, 10, 15, 20];
+
+//* num -> palavrinha auxiliar
+
+let maioresQueDez = numeros.filter(num => num > 10);
+console.log(maioresQueDez); // Retorna -> [15, 20]
+*/
+
+//! Função REDUCE
+// Reduz os valores de um array para um único valor
+/*
+let numeros = [1, 2, 3, 4];
+
+//* Acumulador inicia em 0
+// Variavel auxiliar é o num
+// Acumulador inicia em zero e soma com o auxiliar até o último valor do array
+let soma = numeros.reduce((acumulador, auxiliar) => acumulador + auxiliar, 0);
+
+console.log(soma); // Retorna -> 10
+*/
+
+//! Função FIND
+// Retorna o primeiro elemento que atende a condição
+/*
+let produtos = [
+    {id: 1, nome: "Teclado", preco: 50 }, // produto
+    {id: 2, nome: "Mouse", preco: 30 }, // produto
+    {id: 3, nome: "Caderno", preco: 30 } // produto
+]
+
+//? Diferença de Find e Filter
+// Find -> retorna o primeiro elemento que ele encontra que atende a condição
+// Filter -> retorna todos os elementos que atendem a condição
+
+//* find
+let item = produtos.find(produto => produto.id === 2);
+console.log(item);
+
+//* filter
+let itemfiltrado = produtos.filter(produto => produto.id === 2);
+console.log("Usando filter: " + itemfiltrado);
+*/
+
+//! Função SPLIT
+// Divide a string em partes, transformando num array
+/*
+let frase = "JS é top";
+
+// Com espaço, separa as palavras pelo espaço
+let palavras = frase.split(" ");
+console.log(palavras);
+
+// Sem espaço, separa todas as letras dentro do array
+let letras = frase.split("");
+console.log(letras);
+*/
+
+//! Função TRIM
+// trim remove espaços no inicio e final de uma string
+/*
+let nome = "   Paulo   ";
+let nomeLimpo = nome.trim();
+
+console.log(nome);
+console.log(nomeLimpo);
+*/
+
+//! Função INCLUDE
+// Verifica se existe um valor dentro de uma lista e retorna um booleano
+//* Booleano -> true ou false
+/*
+let frutas = ["maçã", "banana"];
+
+let frutasInclude = frutas.includes("banana");
+console.log(frutasInclude); // Retorna -> true
+*/
+
+//! toLowerCase -> transforma um texto em minúsculo
+//! toUpperCase -> transforma um texto em maiúsculo
+/*
+let nome = "KESSIA";
+let cargo = "instrutora";
+
+console.log(nome.toLowerCase()); // Retorna -> kessia
+console.log(cargo.toUpperCase()); // Retorna -> INSTRUTORA
+*/
+//! Função FOREACH
+/*
+let nomes = ["João", "Guilherme", "Maria"];
+
+nomes.forEach(nome => console.log(`Seu nome é: ${nome}`));
+*/
+
+//! Função SOME e EVERY
+/*
+//* some
+// Verifica se pelo menos um item atende a condição
+// retorna booleano
+
+let numeros = [1, 3, 5, 8];
+
+let temPar = numeros.some(num => num % 2 === 0);
+console.log(temPar); // Retorna -> true
+
+//* every
+// Verifica se TODOS os itens atendem a condição
+// retorna booleano
+
+let todosPares = numeros.every(num => num % 2 === 0);
+console.log(todosPares); // Retorna -> false
+*/
+
+//! Função SORT
+// Ordena os elementos do array
+
+let numeros = [3, 10, 5, 2, 4];
+let letras = ["c", "a", "x", "h"];
+
+//* para letras
+letras.sort();
+console.log(letras); // Retorna -> ["a", "c", "h", "x"]
+
+//* para números
+numeros.sort((a, b) => a - b);
+console.log(numeros); // Retorna -> [2, 3, 4, 5, 10]
